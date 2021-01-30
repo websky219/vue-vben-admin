892efa2845b26b64217012cb100ba6e42557bba2
anncwb <anncwb@users.noreply.github.com>
2021-01-30 08:57:01 +0000

    Automated deployment: Sat Jan 30 08:57:01 UTC 2021 85729f0f40db3fd14984d31263dc5bd39306c28e

--- assets/ChildrenListDetail.926f0687.js
+++
@@ -1,1 +0,0 @@
-import{d as r,o as e,i as a}from"./index.89eaa474.js";import"./vendor.3b1829c7.js";var n=r({});n.render=function(r,n,o,d,i,t){return e(),a("div",null," 子级详情页 ")};export default n;
--- assets/table.3e76ddf2.js
+++
@@ -1,1 +0,0 @@
-import{cT as e}from"./index.89eaa474.js";var r;function a(a){return e.request({url:r.DEMO_LIST,method:"GET",params:a,headers:{ignoreCancelToken:!0}})}(r||(r={})).DEMO_LIST="/table/getDemoList";export{a as d};
